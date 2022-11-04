2beb7ef53e6402fc338cf6983359ca574e5f8761
Eliajah Shepard <Eshepard@pgh.yearup.org>
2022-11-04 10:33:28 -0400

    add select , add button, add script source

---
+++ football.js
@@ -0,0 +1,15 @@
+
+//add the event listener for the onload event
+window.onload = function () {
+    let footballTeam = [{code: "DAL", name: "Dallas Cowboys", plays: "Arlington, TX"},
+                        {code: "DEN", name: "DENVER BRONCOS", plays: "Denver, CO"},
+                        {code: "HOU", name: "Houston Texans", plays: "Houston, Texas"},
+                        {code: "KAN", name: "Kansas City Cheifs", plays: "Kansas City, MO"}];
+
+
+// declare the html elemants that we need 
+    let selectMenu = document.getElementById('teams');
+                        console.log(`the select menu is ${selectMenu}`);
+                        let moreInfoBtn = document.getElementById('moreInfo');
+                        console.log(moreInfo);
+}
